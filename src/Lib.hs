2ef8f95d72374599ab940a7ef7e5f82aec4e1a66
Harrison Brewton <harrison@Harrisons-MacBook-Pro.local>
2020-01-22 01:18:49 -0600

    added the database type

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,8 +5,15 @@ module Lib
 import qualified Data.Map as M
 import Control.Monad
 
+type Data = String
 type Id = Int
+type FK = Int -- foreign key
 type TypeSchema = M.Map Id [Id]
+type Table = M.Map FK [Either FK Data]
+
+data Database = Database {
+    tables :: M.Map Id Table
+}
 
 -- down window
 window :: TypeSchema -> Int -> Id -> Maybe [Id]
@@ -15,7 +22,7 @@ window schema depth t = do
     children <- t `M.lookup` schema 
     if 0 == length children
         then Just [t]
-        else fmap join $ sequence $ fmap (window schema (depth-1)) 
+        else fmap join $ sequence $ fmap (window schema (depth-1)) children
         
 -- groethendiek construction
 -- groeth :: TypeSchema -> M.Map
