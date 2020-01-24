6dd5991ef6be3160d392622d1c438e92fc47e69c
Harrison Brewton <hbrewton@wisc.edu>
2020-01-23 19:54:05 -0600

    we good

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,24 +23,23 @@ data Database = Database {
 -- right now this does not take into account possible commutativity that exists in the the DB
 -- for example consider basic image convolution
 kconvolution :: Database -> Id -> Int -> (Either FK Data) -> Maybe [Data]
-kconvolution db _ _ (Right d)  = Just [d]
-kconvolution db _ 0 (Left _)   = Just []
+kconvolution db _ 0  _   = Just [] -- if it is a foreign key, and we're done,don't do anything
+kconvolution db _ _ (Right d)  = Just [d] -- if it is just data, use it
 kconvolution db i k (Left key) = do 
-    schema <- i `M.lookup` (schema db) 
-    table <- i `M.lookup` (tables db) 
-    case (key `M.lookup` table) of
+    schema <- i `M.lookup` (schema db) -- get schema of type
+    table <- i `M.lookup` (tables db) -- get table for type
+    case (key `M.lookup` table) of 
         Just v -> fmap join $ sequence $ zipWith (\i' e' -> kconvolution db i' (k-1) e') schema v
         Nothing -> do
-            sz <- size db i (k+1) -- TODO: need to figure out a better way todo this
+            sz <- size db i k -- note, this does not go down because we pass off to size
             Just $ take sz $ repeat ""
-
-size :: Database -> Id -> Int -> Maybe Int 
-size db i 0 = Just 0
-size db i k = do 
-    entry <- i `M.lookup` (schema db) 
-    if null entry 
-        then Just 1 -- that is it is a final type 
-        else fmap sum $ sequence $ map (\i' -> size db i' (k-1)) entry
+    where 
+        size :: Database -> Id -> Int -> Maybe Int 
+        size db i k = do 
+            entry <- i `M.lookup` (schema db) 
+            if null entry 
+                then Just 1 -- that is it is a final type 
+                else (if k /= 0 then fmap sum $ sequence $ map (\i' -> size db i' (k-1)) entry else Just 0)
 
 -- down window
 window :: TypeSchema -> Int -> Id -> Maybe [Id]
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,15 +5,19 @@ import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Control.Monad
 
+
+helper = do 
+    let treeSchema = M.fromList [(0, []), (1, [1, 1, 0])]
+    let dat = M.fromList [(0, M.fromList []), (1, M.fromList [ (43, [Left 44, Left 45, Right "hello"]), (44, [Left 0, Left 0, Right "three"]), (45, [Left 46, Left 47, Right "four"]), (46, [Left 0, Left 0, Right "five"]), (47, [Left 0, Left 0, Right "six"])])]
+    let db = Database treeSchema dat
+    kconvolution db 1 4 (Left 43)
+
 kconv1 :: Bool
 kconv1 = helper == Just ["","","","","","","three","","","five","","","six","four","hello"]
     where 
-        helper = do 
-            let treeSchema = M.fromList [(0, []), (1, [1, 1, 0])]
-            let dat = M.fromList [(0, M.fromList []), (1, M.fromList [ (43, [Left 44, Left 45, Right "hello"]), (44, [Left 0, Left 0, Right "three"]), (45, [Left 46, Left 47, Right "four"]), (46, [Left 0, Left 0, Right "five"]), (47, [Left 0, Left 0, Right "six"])])]
-            let db = Database treeSchema dat
-            kconvolution db 1 4 (Left 43)
 
 tests = [ testGroup "KConv" [testProperty "kconv1" kconv1]]
 
-main = defaultMain tests
+main = do 
+    putStrLn $ show helper
+    defaultMain tests
